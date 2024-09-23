045cfed9f3c689332166cd8727ebeae993d64a6a
ivan0525 <zhuangyufeng189@gmail.com>
2024-09-24 00:24:07 +0800

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,31 @@
-import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
   @Post('create')
   create(@Body() userData: { name?: string; email: string }): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
 
-  @Delete('delete')
+  @Delete('delete/:id')
   delete(@Param('id') id: string): Promise<UserModel> {
-    return this.userService.deleteUser({id: Number(id)});
+    return this.userService.deleteUser({ id: Number(id) });
   }
 
   @Put('publish/:id')
-  async publishPost(@Param('id') id: string,@Body() userData: { name?: string; email: string }): Promise<UserModel> {
+  async publishPost(@Param('id') id: string, @Body() userData: { name?: string; email: string }): Promise<UserModel> {
     return this.userService.updateUser({
       where: { id: Number(id) },
       data: userData,
     });
+  }
+
+  @Get('queryPage')
+  async queryPage(@Query('id') id: string): Promise<UserModel[]> {
+    console.log(id)
+    return this.userService.queryPage({ where: { id: Number(id) } })
   }
 }
