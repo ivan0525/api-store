71484599521b1fd052703e705506eec33d03fc4d
ivan0525 <zhuangyufeng189@gmail.com>
2024-09-25 15:07:26 +0800

    fix(api): test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
+      providers: [UserService],
+      exports: [UserService],
       controllers: [UserController],
     }).compile();
 
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UserController } from './user.controller';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [UserService],
+      exports: [UserService],
+      controllers: [UserController],
     }).compile();
 
     service = module.get<UserService>(UserService);
